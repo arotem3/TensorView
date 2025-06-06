6746ea36d817878954f074285bcdc827c3eeb0bf
arotem3 <arotem33@gmail.com>
2025-06-06 15:53:03 -0600

    TensorTraits in TensorView.hpp

--- TensorView.hpp
+++ TensorView.hpp
@@ -4,6 +4,7 @@
 #include "TensorView/tensorview_config.hpp"
 #include "TensorView/errors.hpp"
 #include "TensorView/ContainerTraits.hpp"
+#include "TensorView/TensorTraits.hpp"
 #include "TensorView/span.hpp"
 #include "TensorView/DynamicTensorShape.hpp"
 #include "TensorView/FixedTensorShape.hpp"
